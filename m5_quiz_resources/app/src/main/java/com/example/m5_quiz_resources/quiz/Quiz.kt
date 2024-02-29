ecd9834b10a8cb52625eea005762b0dfa5fab5c7
AlekhinVladimir <Alehin.v.m@mail.com>
2024-02-29 22:26:28 +0400

    5все еще вожусь

---
+++ m5_quiz_resources/app/src/main/java/com/example/m5_quiz_resources/quiz/Quiz.kt
@@ -0,0 +1,5 @@
+package com.example.m5_quiz_resources.quiz
+
+interface Quiz {
+    val questions: List<Question>
+}
---
+++ m5_quiz_resources/app/src/main/java/com/example/m5_quiz_resources/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.m5_quiz_resources.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
